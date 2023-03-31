9a261c238871e384b4dd7469ac8a3cb8d241d604
mohammed-salem2 <mohammedmazensalem@gmail.com>
2023-03-31 18:03:09 +0300

    imageRoute

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::post('dashboard/test-mission-done', [TestController::class, 'test_mission
 // Custom routes
 Route::name('dashboard.')->prefix('/dashboard')->middleware(['auth'])->group(function() {
     Route::get('/imageLibraryFolder/manage-library', [ImageLibraryFolderController::class, 'manage_library'])->name('imageLibraryFolder.manage-library');
+    Route::delete('imageLibraryFolder/manage-library/{id}', [ImageLibraryFolderController::class, 'delete_image'])->name('imageLibraryFolder.manage-library.destroy');
     Route::resource('/setting' , SettingController::class);
 });
 
