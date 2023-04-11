78dd717825480b3a60294c7ad60ef8b53132c917
mohammed-salem2 <mohammedmazensalem@gmail.com>
2023-04-11 16:53:22 +0300

    test

--- resources/views/dashboard/mission/show.blade.php
+++ resources/views/dashboard/mission/show.blade.php
@@ -24,23 +24,34 @@
                 <div class="col-12 col-md-6 border p-2 d-flex align-items-center">
                     <div class="row w-100 ">
                         <div class="col-3 d-flex justify-content-between align-items-center">
-                            <span class="fw-bold">Description</span>
-                            <span>:</span>
-                        </div>
-                        <div class="col-9 d-flex justify-content-between align-items-center p-0">
-                            <span>{{ $model['description'] }}</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 border p-2 d-flex align-items-center">
-                    <div class="row w-100 ">
-                        <div class="col-3 d-flex justify-content-between align-items-center">
                             <span class="fw-bold">Is Active</span>
                             <span>:</span>
                         </div>
                         <div class="col-9 d-flex justify-content-between align-items-center p-0">
                             <span
                                 class="badge {{ $model['is_active'] == 1 ? 'bg-success' : 'bg-danger' }}">{{ $model['is_active'] == 1 ? 'Active' : 'Not Active' }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 border p-2 d-flex align-items-center">
+                    <div class="row w-100 ">
+                        <div class="col-3 d-flex justify-content-between align-items-center">
+                            <span class="fw-bold">Description Ar</span>
+                            <span>:</span>
+                        </div>
+                        <div class="col-9 d-flex justify-content-between align-items-center p-0">
+                            <span>{{ $model['description_ar'] }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 border p-2 d-flex align-items-center">
+                    <div class="row w-100 ">
+                        <div class="col-3 d-flex justify-content-between align-items-center">
+                            <span class="fw-bold">Description En</span>
+                            <span>:</span>
+                        </div>
+                        <div class="col-9 d-flex justify-content-between align-items-center p-0">
+                            <span>{{ $model['description_en'] }}</span>
                         </div>
                     </div>
                 </div>
