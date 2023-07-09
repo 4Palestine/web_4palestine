fdba1927fdf6d7ac4f8dd0467e9cc71ffca74257
mohammed-salem2 <mohammedmazensalem@gmail.com>
2023-07-09 19:20:44 +0300

    ContactUsApi

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ContactController;
 use App\Http\Controllers\Api\PlatformController;
 use App\Http\Controllers\Api\HomeController;
 use App\Http\Controllers\Api\MissionController;
 use App\Http\Controllers\Api\UserMissionController;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,18 +32,15 @@ Route::middleware(['auth:sanctum', 'verified', 'checkApiPassword', 'changeLangua
         'mission' => MissionController::class,
         'platform' => PlatformController::class,
     ]);
-
     Route::post('mission-done/{mission_id}', [UserMissionController::class, 'mission_done'])->name('mission_done');
     Route::get('total-stars-of-user/{user_id}', [UserMissionController::class, 'total_stars_of_user'])->name('total_stars_of_user');
     Route::get('top-10-last-week', [UserMissionController::class, 'top_10_last_month'])->name('top_10_last_month');
     Route::get('missions-of-platform/{platform_id}', [MissionController::class, 'missions_of_platform'])->name('missions_of_platform');
-
-
     Route::get('/home', [HomeController::class, 'home'])->name('home');
 });
-
-
-
+Route::get('contact' , [ContactController::class , 'index'])->name('contact_index');
+    Route::post('contact' , [ContactController::class , 'store'])->name('contact_store');
+    Route::delete('contact/{id}/delete' , [ContactController::class , 'destroy'])->name('contact_delete');
 // Super User
 Route::middleware(['auth:sanctum', 'verified', 'checkApiPassword', 'changeLanguage', 'isSuper'])->name('user.')->prefix('user')->group(function () {
 });
