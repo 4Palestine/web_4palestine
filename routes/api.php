76e69c8b1b8ba759654492ac136a6df50f928db2
Khaled Amoudi <67203109+khaled-amoudi@users.noreply.github.com>
2023-07-11 16:20:29 +0300

    FEAT: Contact Dashboard&API | Home Slider Images Dashboard&API | etc..

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\PlatformController;
 use App\Http\Controllers\Api\HomeController;
 use App\Http\Controllers\Api\MissionController;
 use App\Http\Controllers\Api\SettingController;
+use App\Http\Controllers\Api\SliderController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\UserMissionController;
 use App\Models\Contact;
@@ -39,19 +40,26 @@ Route::middleware(['auth:sanctum', 'verified', 'checkApiPassword', 'changeLangua
     Route::get('top-10-last-week', [UserMissionController::class, 'top_10_last_month'])->name('top_10_last_month');
     Route::get('missions-of-platform/{platform_id}', [MissionController::class, 'missions_of_platform'])->name('missions_of_platform');
     Route::get('faqs', [SettingController::class, 'list_of_faqs'])->name('list_of_faqs');
-<<<<<<< HEAD
-=======
+
     Route::get('search-for-mission', [MissionController::class, 'search_for_mission'])->name('search_for_mission');
 
+    // Route::get('contact' , [ContactController::class , 'index'])->name('contact_index');
+    Route::post('contact' , [ContactController::class , 'store'])->name('contact_store');
+    Route::delete('contact/{id}/delete' , [ContactController::class , 'destroy'])->name('contact_delete');
 
     Route::apiResource('profile', UserController::class)->only(['show', 'update']);
-
->>>>>>> 1831e0d (user api)
     Route::get('/home', [HomeController::class, 'home'])->name('home');
+
 });
+<<<<<<< HEAD
 Route::get('contact' , [ContactController::class , 'index'])->name('contact_index');
     Route::post('contact' , [ContactController::class , 'store'])->name('contact_store');
     Route::delete('contact/{id}/delete' , [ContactController::class , 'destroy'])->name('contact_delete');
+    Route::get('slider' , [SliderController::class , 'index'])->name('slider.index');
+=======
+
+
+>>>>>>> a2be4e8 (deploy mohammed salen tasks to main branch)
 // Super User
 Route::middleware(['auth:sanctum', 'verified', 'checkApiPassword', 'changeLanguage', 'isSuper'])->name('user.')->prefix('user')->group(function () {
 });
